171d4a4f9c71b5dfc48e9da645a9a701e5f4d51e
Abasol <44283590+rodulfoabasoloGIT@users.noreply.github.com>
2022-11-06 12:44:52 +0800

    second commit

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,82 +1,82 @@
-import React, { useEffect, useRef } from 'react'
-import { Link } from 'gatsby'
-import ImgBanner from '../../static/book/earthamockup2.png'
-import ImgBanner1 from '../../static/book/book1.png'
+import React, { useEffect, useRef } from "react"
+import { Link } from "gatsby"
+import ImgBanner from "../../static/book/earthamockup2.png"
+import ImgBanner1 from "../../static/book/book1.png"
 // import Slider from 'react-slick'
-// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-import  Aos  from 'aos'
-import "aos/dist/aos.css";
-
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 const Banner = () => {
+  const elemBanner = useRef(null)
 
-    const elemBanner = useRef(null);
+  useEffect(() => {})
 
-    useEffect(() => {
+  // const settings = {
+  //     dots: true,
+  //     infinite: false,
+  //     speed: 500,
+  //     slidesToShow: 1,
+  //     slidesToScroll: 1
+  //   };
 
-    });
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
 
-    // const settings = {
-    //     dots: true,
-    //     infinite: false,
-    //     speed: 500,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    //   };
+  return (
+    <>
+      <section className="banner-section">
+        <div className="grid-two-layout-col">
+          <div className="wrapper-bg-left">
+            <div className="left-col" data-aos="fade-up">
+              <div className="banner-content-wrapper">
+                <h1>
+                  <span className="first-block">
+                    <span id="color-o">
+                      Collaborative Leadership for Classroom and School
+                    </span>
+                  </span>
 
-
-    useEffect(() => {
-        Aos.init({duration:2000});
-      }, []);
-
-    return (
-        <>
-            <section className="banner-section">        
-                <div className="grid-two-layout-col">
-                    <div className="wrapper-bg-left">
-                        <div className="left-col" data-aos="fade-up">
-                            <div className="banner-content-wrapper">
-                                <h1>
-                                    <span className="first-block">
-                                        <span id="color-o">
-                                        Collaborative Leadership for Classroom and School
-                                        </span>
-                                    </span>
-
-                                    {/* <span className="second-block">
+                  {/* <span className="second-block">
                                         <span>
                                             Fog
                                         </span>
                                     </span> */}
-                                </h1>
-                                <p className="p-banner">
-                                Millennials are a difficult bunch. They are disruptive, tardy, disobedient, and quarrelsome. They are 30 percent more likely to be job-hoppers. The cost of retraining them exceeds thirty billion dollars per year, an expense borne ultimately by the larger community. At the same time, research at Social Justice Ireland shows "every person should have a genuine voice in shaping the decisions that affect them."
+                </h1>
+                <p className="p-banner">
+                  Millennials are a difficult bunch. They are disruptive, tardy,
+                  disobedient, and quarrelsome. They are 30 percent more likely
+                  to be job-hoppers. The cost of retraining them exceeds thirty
+                  billion dollars per year, an expense borne ultimately by the
+                  larger community. At the same time, research at Social Justice
+                  Ireland shows "every person should have a genuine voice in
+                  shaping the decisions that affect them."
+                  <blockquote className="block">
+                    “A word of caution – If you are a leader and you are not
+                    seeing that problems are resolved,
+                    <br />
+                    you may be creating them.”
+                    <cite className="cite">- Rodulfo Abasolo</cite>
+                  </blockquote>
+                </p>
 
-                                <blockquote className='block'>
-                                “A word of caution – If you are a leader and you are not seeing that problems are resolved,<br/>you may be creating them.”
-                                <cite className='cite'>- Rodulfo Abasolo</cite>
-                                </blockquote>
-                                </p>
+                <div className="btn-container">
+                  <button className="btn-book">
+                    <Link to="/about-the-book">Read More</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
 
-                                <div className="btn-container">
-                                    <button className="btn-book">
-                                        <Link to="/about-the-book">
-                                            Read More
-                                        </Link>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                        <div className="wrapper-bg-right" data-aos="fade-down">
-                             
-                                <div className="right-col banner-img-col">
-                                            <div >
-                                                <img src={ImgBanner} alt="book" />
-                                            </div>
-                                    {/* <Slider {...settings}> 
+          <div className="wrapper-bg-right" data-aos="fade-down">
+            <div className="right-col banner-img-col">
+              <div>
+                <img src={ImgBanner} alt="book" />
+              </div>
+              {/* <Slider {...settings}> 
                                             <div >
                                                 <img src={ImgBanner} alt="book" />
                                             </div>
@@ -84,13 +84,12 @@ const Banner = () => {
                                                 <img src={ImgBanner1} alt="book1" />
                                             </div>
                                     </Slider> */}
-                                </div>
-                            
-                        </div>
-                </div>
-            </section>
-        </>
-    )
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
-export default Banner;
+export default Banner
--- src/components/parallax-bottom.js
+++ src/components/parallax-bottom.js
@@ -1,37 +1,45 @@
-import React from 'react'
-import {Link} from 'gatsby'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-
+import React from "react"
+import { Link } from "gatsby"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Typewriter from "typewriter-effect"
 
 const ParallaxBottom = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    adaptiveHeight: true,
+    infinite: true,
+  }
 
+  // const tween = KUTE.fromTo(
+  //   "#blob1",
+  //   { path: "#blob1" },
+  //   { path: "#blob2" },
+  //   { repeat: 999, duration: 3000, yoyo: true }
+  // )
+  // tween.start()
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        adaptiveHeight: true,
-        infinite: true,
-    }
-
-    return (
-        <>
-        <section className="parallax-bottom">               
-            <div className="flex-container">
-                <div className="parallax-testimonial">
-                        <h1 data-aos="fade-down">“The terms leadership and problem-solving are interchangeable. <br/>Only those with the courage of that conviction need to read on.”</h1>                                                      
-                </div>
-            </div>
-        </section>
-            
-
-        </>
-    )
+  return (
+    <>
+      <section className="parallax-bottom">
+        <div className="flex-container">
+          <div className="parallax-testimonial">
+            <h1 data-aos="fade-down">
+              “The terms leadership and problem-solving are interchangeable.{" "}
+              <br />
+              Only those with the courage of that conviction need to read on.”
+            </h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default ParallaxBottom
